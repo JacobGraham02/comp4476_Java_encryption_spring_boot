b565caadfb935149da4bb11f9b9c053f31fc9e17
Jacob Graham <jakegraham54@gmail.com>
2023-01-20 20:18:45 -0500

    Added boilerplate folder structure for a Java spring boot application with various preinstalled dependencies

---
+++ src/main/java/com/jacobdgraham/myencrypy/MyencrypyApplication.java
@@ -0,0 +1,13 @@
+package com.jacobdgraham.myencrypy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyencrypyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyencrypyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacobdgraham/myencrypy/MyencrypyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobdgraham.myencrypy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyencrypyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
